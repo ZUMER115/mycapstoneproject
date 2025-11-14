f91faaed8827cfbc97790e15c17483d38ed68375
ZUMER115 <157277943+ZUMER115@users.noreply.github.com>
2025-11-13 23:09:33 -0800

    Delete bin directory

--- bin/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login, verifyEmail } = require('../controllers/authController');
-
-router.post('/register', register);
-router.post('/login', login);
-router.get('/verify', verifyEmail);
-
-module.exports = router;
